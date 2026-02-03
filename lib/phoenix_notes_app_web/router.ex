a8f98da87ce28ea88c1131a1710261401583ba5e
Dean <deanlt.santos@gmail.com>
2026-02-03 08:00:37 +0800

    added note controller to scope

--- lib/phoenix_notes_app_web/router.ex
+++ lib/phoenix_notes_app_web/router.ex
@@ -18,6 +18,7 @@ defmodule PhoenixNotesAppWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
+    get "/notes", NoteController, :index
   end
 
   # Other scopes may use custom stacks.
