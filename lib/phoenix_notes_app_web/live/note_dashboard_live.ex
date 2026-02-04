a1c5aa03fa3f8066a990755687540f031a64af8c
Dean <deanlt.santos@gmail.com>
2026-02-05 03:03:03 +0800

    added the event logic for opening the create_note feature

--- lib/phoenix_notes_app_web/live/note_dashboard_live.ex
+++ lib/phoenix_notes_app_web/live/note_dashboard_live.ex
@@ -1,7 +1,7 @@
 defmodule PhoenixNotesAppWeb.NoteDashboardLive do
   use PhoenixNotesAppWeb, :live_view
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, show_modal: false)}
+    {:ok, assign(socket, show_modal: false, show_create_note: false)}
   end
 
   def handle_event("open-modal", _, socket) do
@@ -11,9 +11,18 @@ defmodule PhoenixNotesAppWeb.NoteDashboardLive do
   def handle_event("close-modal", _, socket) do
     {:noreply, assign(socket, show_modal: false)}
   end
+
+  def handle_event("open-create-note-modal", _, socket) do
+    {:noreply, assign(socket, show_create_note: true)}
+  end
+
+  def handle_event("close-create-note", _, socket) do
+    {:noreply, assign(socket, show_create_note: false)}
+  end
+
   def render(assigns) do
   ~H"""
-    <.dashboard show_modal={@show_modal}/>
+    <.dashboard show_modal={@show_modal} show_create_note={@show_create_note}/>
   """
   end
   embed_templates "layouts/note_dashboard_html/*"
