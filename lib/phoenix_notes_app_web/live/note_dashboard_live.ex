5889fae4323d2471d3a80789edb4db42dbf089d3
Dean <deanlt.santos@gmail.com>
2026-02-05 02:11:43 +0800

    implemented the logic for viewing the note and passed the value to the parent view through a prop

--- lib/phoenix_notes_app_web/live/note_dashboard_live.ex
+++ lib/phoenix_notes_app_web/live/note_dashboard_live.ex
@@ -1,12 +1,19 @@
 defmodule PhoenixNotesAppWeb.NoteDashboardLive do
   use PhoenixNotesAppWeb, :live_view
   def mount(_params, _session, socket) do
-  {:ok, assign(socket, :brightness, 10)}
+    {:ok, assign(socket, show_modal: false)}
   end
 
+  def handle_event("open-modal", _, socket) do
+    {:noreply, assign(socket, show_modal: true)}
+  end
+
+  def handle_event("close-modal", _, socket) do
+    {:noreply, assign(socket, show_modal: false)}
+  end
   def render(assigns) do
   ~H"""
-    <.dashboard />
+    <.dashboard show_modal={@show_modal}/>
   """
   end
   embed_templates "layouts/note_dashboard_html/*"
