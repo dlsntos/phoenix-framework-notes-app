9eb862fa0fd71a9392688bbed9548acb29a53c93
Dean <deanlt.santos@gmail.com>
2026-02-06 07:39:53 +0800

    create login controller logic

--- lib/phoenix_notes_app_web/controllers/login_controller.ex
+++ lib/phoenix_notes_app_web/controllers/login_controller.ex
@@ -1,6 +1,32 @@
 defmodule PhoenixNotesAppWeb.LoginController do
   use PhoenixNotesAppWeb, :controller
+  import Phoenix.Component
+  alias PhoenixNotesApp.Users
+  alias PhoenixNotesApp.Users.User
+
   def login(conn, _params) do
-    render(conn, :login, layout: false)
+    changeset = Users.change_user(%User{})
+    render(conn, :login, form: to_form(changeset),layout: false)
+  end
+
+  def create(conn, %{"user" => user_params}) do
+    case Users.authenticate_user(user_params) do
+      {:ok, user} ->
+        conn
+        |> put_session(:user_id, user.id)
+        |> configure_session(renew: true)
+        |> redirect(to: ~p"/notes")
+        |> halt()
+
+      {:error, _reason} ->
+        changeset =
+          %User{}
+          |> Users.change_user(user_params)
+          |> Map.put(:action, :insert)
+
+        conn
+        |> put_flash(:error, "Invalid email or password")
+        |> render(:login, form: to_form(changeset), layout: false)
+    end
   end
 end
