d26e8a0decc9138607ba2f6bc0c2e17abd3592c8
Dean <deanlt.santos@gmail.com>
2026-02-06 07:39:23 +0800

    create context for user

--- lib/phoenix_notes_app/users.ex
+++ lib/phoenix_notes_app/users.ex
@@ -3,21 +3,25 @@ defmodule PhoenixNotesApp.Users do
   alias PhoenixNotesApp.Repo
   alias PhoenixNotesApp.Users.User
 
-  # Login function
-  def login_user(username_or_email, password) do
-    user =
-      Repo.get_by(User, username: username_or_email) ||
-      Repo.get_by(User, email: username_or_email)
+  def change_user(%User{} = user, attrs \\ %{}) do
+    User.changeset(user, attrs)
+  end
 
-    cond do
-      user && user.haspass == password ->
-        {:ok, user}
+  defp get_user_by_email(email) do
+    Repo.get_by(User, email: email)
+  end
+
+  def authenticate_user(%{"email" => email, "password" => password}) do
+    case get_user_by_email(email) do
+      nil ->
+        {:error, :invalid_credentials}
 
       user ->
-        {:error, :invalid_password}
-
-      true ->
-        {:error, :user_not_found}
+        if user.hashed_password == password do
+          {:ok, user}
+        else
+          {:error, :invalid_credentials}
+        end
     end
   end
 end
