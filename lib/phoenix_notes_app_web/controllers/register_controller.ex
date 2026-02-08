3579b63145003ea87fe2a798d62f142f4309a854
Dean <deanlt.santos@gmail.com>
2026-02-09 02:11:08 +0800

    add register logic to register controller

--- lib/phoenix_notes_app_web/controllers/register_controller.ex
+++ lib/phoenix_notes_app_web/controllers/register_controller.ex
@@ -1,7 +1,24 @@
 defmodule PhoenixNotesAppWeb.RegisterController do
   use PhoenixNotesAppWeb, :controller
+  import Phoenix.Component
+  alias PhoenixNotesApp.Users
+  alias PhoenixNotesApp.Users.User
 
   def register(conn, _params) do
-    render(conn, :register, layout: false)
+    changeset = Users.change_user(%User{})
+    render(conn, :register, form: to_form(changeset),layout: false)
+  end
+
+  @spec create(Plug.Conn.t(), map()) :: Plug.Conn.t()
+  def create(conn, %{"user" => user_params}) do
+    case Users.create_user(user_params) do
+      {:ok, _user} ->
+        conn
+        |> put_flash(:info, "User created successfully")
+        |> redirect(to: ~p"/login")
+
+      {:error, changeset} ->
+      render(conn, :register, form: to_form(changeset), layout: false)
+    end
   end
 end
