4e1dd19a9b8e04b39fed8c1d88a5ba88f57cbb2a
Dean <deanlt.santos@gmail.com>
2026-02-11 05:25:47 +0800

    configured the auth into the current scope of the layout where the dashboard is placed

--- lib/phoenix_notes_app_web/user_auth.ex
+++ lib/phoenix_notes_app_web/user_auth.ex
@@ -11,12 +11,12 @@ defmodule PhoenixNotesAppWeb.UserAuth do
 
     case session["user_id"] do
       nil ->
-        {:cont, assign(socket, :current_user, nil)}
+        {:cont, assign(socket, :current_user, current_scope: nil)}
 
       user_id ->
         case Users.get_user(user_id) do
-          nil -> {:cont, assign(socket, :current_user, nil)}
-          user -> {:cont, assign(socket, :current_user, user)}
+          nil -> {:cont, assign(socket, current_user: nil, current_scope: nil)}
+          user -> {:cont, assign(socket, current_user: user, current_scope: user)}
         end
     end
   end
