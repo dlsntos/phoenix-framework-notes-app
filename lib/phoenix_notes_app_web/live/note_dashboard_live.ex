d6fdba468a0dbbd9d1f9e9518f9a7bfbe013d22f
Dean <deanlt.santos@gmail.com>
2026-02-11 08:10:47 +0800

    move the note_item view and changed it from a live_component to a private function component

--- lib/phoenix_notes_app_web/live/note_dashboard_live.ex
+++ lib/phoenix_notes_app_web/live/note_dashboard_live.ex
@@ -185,8 +185,7 @@ defmodule PhoenixNotesAppWeb.NoteDashboardLive do
 
       <section class="mt-10 grid grid-cols-1 sm:grid-cols-[repeat(2,minmax(0,250px))] md:grid-cols-[repeat(3,minmax(0,300px))] lg:grid-cols-[repeat(4,minmax(0,350px))] justify-center items-center auto-rows-[15rem] gap-5">
         <%= for note <- @notes do %>
-          <.live_component
-            module={PhoenixNotesAppWeb.NoteDashboardLive.NoteItemComponent}
+          <.note_item_component
             id={"note-item-#{note.id}"}
             note={note}
           />
@@ -222,4 +221,36 @@ defmodule PhoenixNotesAppWeb.NoteDashboardLive do
   """
   end
 
+  defp note_item_component(assigns) do
+  ~H"""
+    <div
+      class="flex flex-col h-full max-h-[250px] bg-white rounded-xl drop-shadow-md transition duration-300 hover:scale-105"
+    >
+      <!--Note title section-->
+      <section class="flex bg-[var(--bg-lightorange)] p-3 text-[var(--text-white-1)] rounded-t-2xl">
+      <h2 class="mx-auto text-xl font-semibold font-[var(--font-montserrat)] drop-shadow-md">
+        {@note.title}
+      </h2>
+      </section>
+
+      <!--Note content section-->
+      <section class="relative h-full max-h-[120px] py-5 text-center overflow-hidden">
+        <p class="h-full max-h-[100px] w-full max-w-md md:max-w-xs text-lg text-justify text-gray-500 font-[var(--font-comme)] px-5 drop-shadow-sm break-words overflow-hidden">
+          {@note.content}
+        </p>
+        <div class="pointer-events-none absolute bottom-0 left-0 w-full h-10 bg-gradient-to-t from-white/100 to-white/0"></div>
+      </section>
+
+      <!--Open note button section-->
+      <section class="flex justify-end p-3 rounded-b-xl z-40">
+        <div
+          phx-click="open-modal"
+          phx-value-id={@note.id}
+          class="h-10 w-10 py-2 bg-[var(--bg-lightorange)] rounded-full cursor-pointer transition duration-200 hover:bg-orange-700 hover:scale-110 cursor-pointer">
+          <.icon name="hero-arrows-pointing-out" class="ml-2 mb-1 size-6 text-[var(--text-white-1)]"/>
+        </div>
+      </section>
+    </div>
+  """
+  end
 end
