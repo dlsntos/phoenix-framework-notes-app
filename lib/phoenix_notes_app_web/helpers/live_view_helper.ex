3c326f3ae2c7837acf40b7f5733cede9a9aeb611
Dean <deanlt.santos@gmail.com>
2026-02-12 05:09:08 +0800

    add a extra parameter to pass in the file module

--- lib/phoenix_notes_app_web/helpers/live_view_helper.ex
+++ lib/phoenix_notes_app_web/helpers/live_view_helper.ex
@@ -1,6 +1,6 @@
 defmodule PhoenixNotesAppWeb.Helpers.LiveViewHelper do
-  def notify_parent(socket, message) do
-    send(self(), {__MODULE__, message})
+  def notify_parent(socket, module, message) when is_atom(module) do
+    send(self(), {module, message})
     socket
   end
 end
