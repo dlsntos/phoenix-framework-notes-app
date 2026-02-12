33a4cac76232860490a2cb649590db543331b982
Dean <deanlt.santos@gmail.com>
2026-02-13 02:53:31 +0800

    add a comment to the hero section

--- lib/phoenix_notes_app_web/view/landing_page_html.ex
+++ lib/phoenix_notes_app_web/view/landing_page_html.ex
@@ -10,6 +10,7 @@ defmodule PhoenixNotesAppWeb.LandingPageHTML do
     </header>
 
     <main class="h-full">
+      <!--Hero Section-->
       <section class="relative flex flex-row justify-center lg:justify-evenly h-full w-full gap-5">
         <img
           src="https://images.unsplash.com/photo-1542435503-956c469947f6?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
