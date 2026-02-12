b2400603c4107f86da19b57b1590ab2384b7902a
Dean <deanlt.santos@gmail.com>
2026-02-13 01:29:45 +0800

    add documentation

--- lib/phoenix_notes_app_web/controllers/landing_page_controller.ex
+++ lib/phoenix_notes_app_web/controllers/landing_page_controller.ex
@@ -1,5 +1,12 @@
 defmodule PhoenixNotesAppWeb.LandingPageController do
   use PhoenixNotesAppWeb, :controller
+
+  @moduledoc """
+  LandingPageController
+
+  ## Purpose
+  Responsible for rendering the Landing Page
+  """
   def index(conn, _params) do
     render(conn, :index, layout: false)
   end
