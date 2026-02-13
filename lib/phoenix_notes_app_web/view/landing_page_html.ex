e7578fcf180206bda04ca16a09fbc35b4b8f300b
Dean <deanlt.santos@gmail.com>
2026-02-13 08:51:05 +0800

    made the target_customer_card reusable and integrated it in the 'who's it for section'

--- lib/phoenix_notes_app_web/view/landing_page_html.ex
+++ lib/phoenix_notes_app_web/view/landing_page_html.ex
@@ -118,9 +118,9 @@ defmodule PhoenixNotesAppWeb.LandingPageHTML do
           <h2 class="py-10 text-center text-6xl font-semibold">Who's it for?</h2>
         </div>
         <div class="self-center max-w-[60vw] grid grid-cols-3 gap-5">
-          <.target_customer_card />
-          <.target_customer_card />
-          <.target_customer_card />
+          <.target_customer_card label={"Student"}/>
+          <.target_customer_card label={"Professionals"}/>
+          <.target_customer_card label={"Creators"}/>
         </div>
       </section>
     </main>
@@ -358,7 +358,9 @@ defmodule PhoenixNotesAppWeb.LandingPageHTML do
   defp target_customer_card(assigns) do
     ~H"""
       <div class="flex flex-row items-center h-auto w-[15rem] py-5 bg-orange-500 rounded-full shadow-md cursor-pointer transition duration-300 hover:bg-orange-700">
-        <p class="mx-auto text-3xl text-white font-semibold">Students</p>
+        <p class="mx-auto text-3xl text-white font-semibold">
+          {@label}
+        </p>
       </div>
     """
   end
