c2414657f0ca46a32e795a9d54a45073daaf35ec
Dean <deanlt.santos@gmail.com>
2026-02-16 04:41:53 +0800

    update moduledoc description

--- lib/phoenix_notes_app.ex
+++ lib/phoenix_notes_app.ex
@@ -1,9 +1,6 @@
 defmodule PhoenixNotesApp do
   @moduledoc """
-  PhoenixNotesApp keeps the contexts that define your domain
-  and business logic.
-
-  Contexts are also responsible for managing your data, regardless
-  if it comes from the database, an external API or others.
+  This program is a notes web app made with the Elixir programming language and Phoenix Fullstack Framework.
+  The Web app includes a landing page and a user dashboard.
   """
 end
