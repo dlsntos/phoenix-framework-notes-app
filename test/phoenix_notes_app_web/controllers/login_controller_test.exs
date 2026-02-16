fa5e9637014c4cea0f323e52558b9eb1aa2c1282
Dean <deanlt.santos@gmail.com>
2026-02-17 03:37:27 +0800

    implement unit test on login with correct credentials

--- test/phoenix_notes_app_web/controllers/login_controller_test.exs
+++ test/phoenix_notes_app_web/controllers/login_controller_test.exs
@@ -6,7 +6,7 @@ defmodule PhoenixNotesAppWeb.LoginControllerTest do
   defp reg_user(attrs \\ %{}) do
     {:ok, user} =
       attrs
-      |> Enum.into(%{username: "Test user", email: "iex@example.com", password: "secret123"})
+      |> Enum.into(%{username: "Test user", email: "iex@example.com", hashed_password: "secret123"})
       |> Users.create_user()
 
     user
@@ -15,5 +15,17 @@ defmodule PhoenixNotesAppWeb.LoginControllerTest do
   test "GET /login", %{conn: conn} do
     conn = get(conn, ~p"/login")
     assert html_response(conn, 200)
+  end
+
+  test "POST /login with valid credentials", %{conn: conn} do
+    user = reg_user()
+
+    conn =
+      post(conn, ~p"/login", %{
+        "user" => %{"email" => user.email, "password" => "secret123"}
+      })
+
+    assert redirected_to(conn) == ~p"/notes"
+    assert get_session(conn, :user_id) == user.id
   end
 end
