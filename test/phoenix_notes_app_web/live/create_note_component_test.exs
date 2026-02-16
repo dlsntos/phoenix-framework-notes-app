c12c855b3d859231c7353e55469c211de7ab7f2b
Dean <deanlt.santos@gmail.com>
2026-02-17 07:30:53 +0800

    create create_note_component test

---
+++ test/phoenix_notes_app_web/live/create_note_component_test.exs
@@ -0,0 +1,8 @@
+defmodule PhoenixNotesAppWeb.CreateNoteComponentTest do
+  use PhoenixNotesAppWeb.ConnCase, async: true
+  use ExUnit.Case, async: true
+  import Phoenix.LiveViewTest
+
+  alias PhoenixNotesApp.Users
+  alias PhoenixNotesApp.Notes
+end
