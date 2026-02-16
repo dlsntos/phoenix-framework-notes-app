3118a324e6d5b09d7624fe5e8dfad56c9a1114b7
Dean <deanlt.santos@gmail.com>
2026-02-17 07:37:23 +0800

    create create_note_component test

--- test/phoenix_notes_app_web/live/create_note_component_test.exs
+++ test/phoenix_notes_app_web/live/create_note_component_test.exs
@@ -5,4 +5,32 @@ defmodule PhoenixNotesAppWeb.CreateNoteComponentTest do
 
   alias PhoenixNotesApp.Users
   alias PhoenixNotesApp.Notes
+
+  defp create_user do
+    attrs = %{
+      "username" => "alice",
+      "email" => "alice@example.com",
+      "hashed_password" => "secret123"
+    }
+
+    {:ok, user} = Users.create_user(attrs)
+    user
+  end
+
+  defp create_note(user, attrs \\ %{}) do
+    attrs =
+      Map.merge(
+        %{"title" => "First note", "content" => "hello", "user_id" => user.id},
+        attrs
+      )
+
+    {:ok, note} = Notes.create_note(attrs)
+    note
+  end
+
+  defp log_in(conn, user) do
+    init_test_session(conn, %{"user_id" => user.id})
+  end
+
+
 end
