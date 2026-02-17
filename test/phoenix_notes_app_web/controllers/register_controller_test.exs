06b42dee8099125c7aa02a75d1a60277b9ef3403
Dean <deanlt.santos@gmail.com>
2026-02-17 09:48:14 +0800

    implement unit test on register function

--- test/phoenix_notes_app_web/controllers/register_controller_test.exs
+++ test/phoenix_notes_app_web/controllers/register_controller_test.exs
@@ -5,4 +5,12 @@ defmodule PhoenixNotesAppWeb.RegisterControllerTest do
     conn = get(conn, ~p"/register")
     assert html_response(conn, 200)
   end
+
+  test "POST /register, register with complete credentials", %{conn: conn} do
+    conn = post(conn, ~p"/register", %{
+      "user" => %{"username" => "test user", "email" => "iex@example.com", "hashed_password" => "secret"}
+    })
+
+    assert redirected_to(conn) == ~p"/login"
+  end
 end
