36750d8b41621abaf0c5b8d662b3dff12d34f2a0
Dean <deanlt.santos@gmail.com>
2026-02-17 09:05:34 +0800

    add comments

--- lib/phoenix_notes_app_web/live/view_note_component.ex
+++ lib/phoenix_notes_app_web/live/view_note_component.ex
@@ -105,6 +105,8 @@ defmodule PhoenixNotesAppWeb.NoteDashboardLive.ViewNoteComponent do
       id="view-note-modal"
       class="fixed top-0 left-0 flex flex-row justify-center items-center h-screen w-full bg-black/70 z-10000 overflow-y-hidden">
       <section class="h-full max-h-[80vh] w-full max-w-sm md:max-w-3xl rounded-box bg-white drop-shadow-2xl">
+
+        <!-- View note modal header-->
         <div class="flex flex-col h-auto w-full p-2 bg-orange-500 rounded-t-lg drop-shadow-sm">
           <button
             class="flex flex-row self-end items-center h-10 w-10 bg-red-500 rounded-full cursor-pointer transition duration-200 hover:bg-red-700 hover:scale-110"
@@ -116,6 +118,7 @@ defmodule PhoenixNotesAppWeb.NoteDashboardLive.ViewNoteComponent do
           </button>
 
           <div class="text-center">
+            <!-- Heading changes depending on the condition if the note is being viewed or edit -->
             <h1 class="text-2xl md:text-4xl text-white font-bold text-gray-800 text-shadow-sm">
               <%= if @edit_mode do %>
                 Editing note
@@ -132,6 +135,7 @@ defmodule PhoenixNotesAppWeb.NoteDashboardLive.ViewNoteComponent do
           </div>
         </div>
 
+        <!-- Conditional rendering for edit_mode and view_note_content -->
         <%= if @edit_mode do %>
           <.edit_note
             note={@note}
