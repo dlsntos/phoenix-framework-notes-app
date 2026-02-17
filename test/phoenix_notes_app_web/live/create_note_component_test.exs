e571a85546dbc27cb6517bbb0825a18538587631
Dean <deanlt.santos@gmail.com>
2026-02-18 01:37:02 +0800

    implement unit test on create new note

--- test/phoenix_notes_app_web/live/create_note_component_test.exs
+++ test/phoenix_notes_app_web/live/create_note_component_test.exs
@@ -1,6 +1,5 @@
 defmodule PhoenixNotesAppWeb.CreateNoteComponentTest do
   use PhoenixNotesAppWeb.ConnCase, async: true
-  use ExUnit.Case, async: true
   import Phoenix.LiveViewTest
 
   alias PhoenixNotesApp.Users
@@ -20,10 +19,34 @@ defmodule PhoenixNotesAppWeb.CreateNoteComponentTest do
     init_test_session(conn, %{"user_id" => user.id})
   end
 
+  test "create a new note", %{conn: conn} do
+    user = create_user()
+    {:ok, view, _html} =
+      conn
+      |> log_in(user)
+      |> live(~p"/notes")
+
+    view
+    |> element("#open-create-note-modal")
+    |> render_click()
+
+    assert has_element?(view, "#show-create-note-modal")
+
+    view
+    |> element("#create-note-form")
+    |> render_submit(%{"note" => %{
+      "title" => "Grocery List",
+      "content" => "1. eggs\n2. flour\n3. coffee"
+    }})
+
+    notes = PhoenixNotesApp.Notes.get_all_notes_by_userid(user.id)
+    assert Enum.any?(notes, fn n -> n.title == "Grocery List" end)
+  end
+
   test "close create note modal", %{conn: conn} do
     user = create_user()
 
-    {:ok, view, _html} =
+    {:ok, view, html} =
       conn
       |> log_in(user)
       |> live(~p"/notes")
